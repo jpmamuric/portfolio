50657b64bb524fe4d4110851a9c083a4405ec58b
Jan Peter Mamuric <jmamuric@jans-mbp.attlocal.net>
2017-03-07 00:10:30 -0800

    fixed react router error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import './index.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -8,6 +8,6 @@ injectTapEventPlugin();
 import routes from './routes/routes';
 
 ReactDOM.render(
-  <Router routes={routes} history={browserHistory}/>,
+  <Router routes={routes} history={hashHistory}/>,
   document.getElementById('root')
 );
